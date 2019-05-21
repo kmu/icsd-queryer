e4061d5324f50f7aaf8404362738377259cda158
Koki Muraoka <koki.muraoka@gmail.com>
2019-05-21 11:09:32 -0700

    Use unittest

--- tests/test_queryer.py
+++ tests/test_queryer.py
@@ -1,12 +1,17 @@
-import queryer
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from queryer import Queryer
+import unittest
 
-query = {
-    'composition': 'Ni:1:1 Ti:2:2',
-    'number_of_elements': '2'
-    }
-##query = {'icsd_collection_code': 181801}
-queryer = queryer.Queryer(query=query)
-queryer.perform_icsd_query()
-##print queryer.hits
-##queryer.quit()
 
+class TestQueryer(unittest.TestCase):
+    def test_NiTi(self):
+        query = {
+            'composition': 'Ni:1:1 Ti:2:2',
+            'number_of_elements': '2'
+        }
+        ##query = {'icsd_collection_code': 181801}
+        queryer = Queryer(query=query)
+        queryer.perform_icsd_query()
+        # print queryer.hits
+        # queryer.quit()
