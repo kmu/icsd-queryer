03d342668729cbf2fb66ac23770a797bd44dff58
Koki Muraoka <koki.muraoka@gmail.com>
2019-06-05 16:27:04 -0700

    Test for theoretical structures

--- tests/test_queryer.py
+++ tests/test_queryer.py
@@ -150,6 +150,39 @@ class TestQueryer(unittest.TestCase):
         with open("{}/meta_data.json".format(code)) as f:
             crawled_dict = json.load(f)
 
+        self.assertEqual("Powder Diffraction (1987) 2, p225-p226", expected_dict['reference_2'])
+        self.assertEqual("", crawled_dict['reference_2'])
+
+        for key in new_keys + conflicting_keys:
+            del crawled_dict[key]
+
+        for key in abolished_keys + conflicting_keys:
+            del expected_dict[key]
+
+        self.assertDictEqual(expected_dict, crawled_dict)
+
+
+    @unittest.skipIf(not is_mac, "Use macOS to run this")
+    def test_theory(self):
+        """
+        ICSD ver. 2017 had multiple references,
+        while ICSD ver. 2019 does not have.
+        """
+        code = 195347
+        query = {
+            "icsd_collection_code": code,
+        }
+
+        queryer = Queryer(query=query, structure_source="theory")
+        queryer.perform_icsd_query()
+        self.assertEqual(1, queryer.hits)
+
+        with open("expected/{}/meta_data.json".format(code)) as f:
+            expected_dict = json.load(f)
+
+        with open("{}/meta_data.json".format(code)) as f:
+            crawled_dict = json.load(f)
+
         for key in new_keys + conflicting_keys:
             del crawled_dict[key]
 
