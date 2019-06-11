4d26fdebf930774726543e9d41de81aec2c56c1c
Koki Muraoka <koki.muraoka@gmail.com>
2019-06-11 14:01:31 -0700

    Add metadata

---
+++ metadata.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+version = '0.0.1'
+project = 'icsd-queryer'
