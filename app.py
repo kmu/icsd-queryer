8bd0074f14c30b55b8674bf2fbdaa2daa725feb3
Koki Muraoka <koki.muraoka@gmail.com>
2019-06-11 11:55:00 -0700

    Add metadata

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
+import argparse
+from metadat import *
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    parser.add_argument(
+        '-V', '--version',
+        action='version',
+        version='{0} {1}'.format(metadata.project, metadata.version))
+    parser_add = subparsers.add_parser('scrape', help='scrape ICSD')
+    parser_add.add_argument('-A', '--all', action='store_true', help='scrape everything using ICSD Collection Code')
+    parser_add.add_argument('--composition', help='scrape everything using ICSD Collection Code', default=None, type=str)
+    parser_add.add_argument('--code', help='scape by ICSD Collection Code (e.g. 2000, 2000-2050)', default=None, type=str)
+    parser_add.set_defaults(handler=command_add)
+
+    parser_enumerate = subparsers.add_parser('enumerate', help='make list of all ICSD codes')
+
+    parser_ls = subparsers.add_parser('ls', help='report already retrieved entries')
+    parser_ls.add_argument('--code', help='ICSD Collection Code (e.g. 2000, 2000-2050)', default=None, type=str)
--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
 setup(
+    name="icsd",
     install_requires=["PyYAML","beautifulsoup4", "selenium", "PyVirtualDisplay", "pandas"],
     entry_points={
         "console_scripts": [
             "icsd = app:main"
         ]
-    }
+    },
+    maintainer="Koki Muraoka",
+    maintainer_email="KMuraoka@lbl.gov",
 )
