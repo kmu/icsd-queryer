88e05cb3decd47d811dc9a9ee3b39205233b8095
Koki Muraoka <koki.muraoka@gmail.com>
2019-06-12 11:35:38 -0700

    Requires tqdm

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     version='0.0.2',
     name='icsd-queryer',
-    install_requires=["PyYAML","beautifulsoup4", "selenium", "PyVirtualDisplay", "pandas"],
+    install_requires=["PyYAML","beautifulsoup4", "selenium", "PyVirtualDisplay", "pandas", "tqdm"],
     entry_points={
         "console_scripts": [
             "icsd = app:main"
