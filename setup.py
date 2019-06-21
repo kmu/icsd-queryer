d760e0e322589e89d7ff68bf04528f0a2f86729e
Koki Muraoka <kmu@cori08.nersc.gov>
2019-06-21 10:07:25 -0700

    Version up

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    version='0.0.2',
+    version='0.0.3',
     name='icsd',
     install_requires=["PyYAML","beautifulsoup4", "selenium", "PyVirtualDisplay", "pandas", "tqdm"],
     entry_points={
